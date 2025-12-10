1914688801b2a5c049cd74a82a57024ad3271c41
Paulo Pilotti Duarte <guilhermepilotti@gmail.com>
2025-12-10 20:13:18 -0300

    Add files.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Street Fighter 6 Companion',
+  webDir: 'www'
+};
+
+export default config;
